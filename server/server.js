94da14b8d99aba4e2fc05ddfb87069350ee7e2cf
unknown <bikerstevefitness@gmail.com>
2018-05-10 12:00:23 -0600

    testing

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const express          = require('express'),
 
 const app = express()
 
-// app.use(express.static(__dirname + '/../build'))
+app.use(express.static(__dirname + '/../build'))
 app.use(express.json())
 app.use(cors())
 // Setting up express-session
